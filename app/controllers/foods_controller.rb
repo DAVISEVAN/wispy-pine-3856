1a0438c027e60f98d0c81db5d173a55a9e3c6ff9
Evan Davis <evandavis427@gmail.com>
2024-09-06 10:28:56 -0600

    refactored but need to write tests for poro, facade, service, and update initial test

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,19 +1,22 @@
 class FoodsController < ApplicationController
   def index
-    ingredient = params[:ingredient]
-    if ingredient.present?
-      response = Faraday.get("https://api.nal.usda.gov/fdc/v1/foods/search") do |req|
-        req.params['api_key'] = Rails.application.credentials.usda[:api_key]
-        req.params['query'] = ingredient
-        req.params['pageSize'] = 10
-      end
-
-      data = JSON.parse(response.body)
-      @foods = data['foods']
-      @total_results = data['totalHits']
-    else
-      @foods = []
-      @total_results = 0
-    end
+    @foods = FoodSearchFacade.search(params[:ingredient])
+    @total_results = FoodSearchFacade.total_results(params[:ingredient])
   end
+  #   ingredient = params[:ingredient]
+  #   if ingredient.present?
+  #     response = Faraday.get("https://api.nal.usda.gov/fdc/v1/foods/search") do |req|
+  #       req.params['api_key'] = Rails.application.credentials.usda[:api_key]
+  #       req.params['query'] = ingredient
+  #       req.params['pageSize'] = 10
+  #     end
+
+  #     data = JSON.parse(response.body)
+  #     @foods = data['foods']
+  #     @total_results = data['totalHits']
+  #   else
+  #     @foods = []
+  #     @total_results = 0
+  #   end
+  # end
 end
